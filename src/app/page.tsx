47423df5f4d4db083e30794e98a2d9c556ca4a3c
Kevin <ks.hernandez418@gmail.com>
2025-07-08 21:02:02 -0500

    dialog proof of concept

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,16 @@
+'use client'
 import Logo from '@/components/Icons/LOGO.svg'
+import { X } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeading,
+  DialogTrigger,
+  useDialogContext,
+} from './dialog/Dialog'
 const fakeData = [
   {
     title: 'Recomendados',
@@ -53,6 +63,23 @@ const fakeData = [
   //   items: [{ title: 'Platos', href: '/novedades/platos' }],
   // },
 ]
+
+function DialogCloseIcon() {
+  const { setOpen } = useDialogContext()
+  const handleClick = (e: React.MouseEvent) => {
+    e.stopPropagation() // 🔥 prevent bubbling to parent trigger
+    setOpen(false)
+  }
+  return (
+    <button
+      onClick={handleClick}
+      className=" text-gray-500 hover:text-black"
+      aria-label="Close"
+    >
+      <X className="w-5 h-5" />
+    </button>
+  )
+}
 export default function Home() {
   return (
     <div className="flex flex-col bg-surface-2 items-center justify-center h-screen gap-10 text-white">
@@ -60,7 +87,7 @@ export default function Home() {
 
       <div>
         <div className="grid grid-cols-2 gap-8 mt-4">
-          {fakeData.map((column, index) => (
+          {/* {fakeData.map((column, index) => (
             <Link
               href={column.items[0].href}
               className={`flex  h-52 w-52 justify-center text-wrap flex-row  font-bold items-center uppercase rounded-xl ${column.color} p-3`}
@@ -68,7 +95,40 @@ export default function Home() {
             >
               <span className="text-center">{column.title}</span>
             </Link>
-          ))}
+          ))} */}
+          <Dialog>
+            <DialogTrigger>
+              <div className="bg-red-100 h-56 w-72 font-black flex flex-col items-center text-black">
+                <span>Parent</span>
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <div
+                      onClick={(e) => e.stopPropagation()}
+                      className="bg-red-500"
+                    >
+                      Open nested dialog
+                    </div>
+                  </DialogTrigger>
+                  <DialogContent className="Dialog">
+                    <DialogCloseIcon />
+                    <DialogHeading>
+                      My dialog heading nestedddddddd
+                    </DialogHeading>
+                    <DialogDescription>
+                      <span className="relative">
+                        <span>My dialog description nestedddddd</span>
+                      </span>
+                    </DialogDescription>
+                  </DialogContent>
+                </Dialog>
+              </div>
+            </DialogTrigger>
+            <DialogContent className="Dialog">
+              <DialogCloseIcon />
+              <DialogHeading>My dialog heading</DialogHeading>
+              <DialogDescription>My dialog description</DialogDescription>
+            </DialogContent>
+          </Dialog>
         </div>
         <Link
           href={'/novedades/platos'}
