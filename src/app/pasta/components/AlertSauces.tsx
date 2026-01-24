ca3afef550e17622de25c3f0c500a231205fa813
Kevin <ks.hernandez418@gmail.com>
2026-01-24 16:57:01 -0500

    fix allergens pop up

--- src/app/pasta/components/AlertSauces.tsx
+++ src/app/pasta/components/AlertSauces.tsx
@@ -9,12 +9,20 @@ import OverlayPopup from '@/components/Dialog/OverlayPopup'
 import { useState } from 'react'
 
 const iconsToMap = [
-  { Icon: SojaRestrictionIcon, alt: 'Soja Restriction Icon' },
-  { Icon: MilkRestrictionIcon, alt: 'Milk Restriction Icon' },
-  { Icon: EggsRestrictionIcon, alt: 'Eggs Restriction Icon' },
-  { Icon: GlutenRestrictionIcon, alt: 'Gluten Restriction Icon' },
-  { Icon: CeleryRestrictionIcon, alt: 'Celery Restriction Icon' },
-  { Icon: SulphiteRestrictionIcon, alt: 'Sulphite Restriction Icon' },
+  { Icon: SojaRestrictionIcon, alt: 'Soja Restriction Icon', name: 'Soja' },
+  {
+    Icon: GlutenRestrictionIcon,
+    alt: 'Gluten Restriction Icon',
+    name: 'Gluten',
+  },
+  { Icon: MilkRestrictionIcon, alt: 'Milk Restriction Icon', name: 'Lacteos' },
+  { Icon: CeleryRestrictionIcon, alt: 'Celery Restriction Icon', name: 'Apio' },
+  { Icon: EggsRestrictionIcon, alt: 'Eggs Restriction Icon', name: 'Huevo' },
+  {
+    Icon: SulphiteRestrictionIcon,
+    alt: 'Sulphite Restriction Icon',
+    name: 'Sulfitos',
+  },
 ]
 
 interface AlertSaucesProps {
@@ -28,18 +36,25 @@ const AlertSauces = ({ onClose }: AlertSaucesProps) => {
   }
   return (
     <OverlayPopup open={open} onClose={onCloseDialog}>
-      <div className="h-full w-full justify-center items-center flex  ">
+      <div className="h-full w-full justify-center items-center flex">
         <div
-          className={`p-5  bg-white/80 backdrop-blur-sm uppercase  md:w-[26rem] w-[23rem] px-10 ${'border-2 border-red-600'} rounded-2xl text-center shadow-lg relative`}
+          className={`p-5  bg-white/80 backdrop-blur-sm uppercase  md:w-[30rem] w-[24rem] px-10 ${'border-2 border-red-600'} rounded-2xl text-center shadow-lg relative`}
         >
           <div>
             <span>
               No se recomienda consumir pasta si el cliente tiene alergia a:
             </span>
-            <div className="flex gap-4 mx-auto items-center justify-center my-4">
-              {iconsToMap.map(({ Icon, alt }, index) => (
-                <Icon key={index} />
+            <div className=" gap-4 grid grid-cols-2 items-center justify-center my-4 w-56 mx-auto">
+              {iconsToMap.map(({ Icon, name }, index) => (
+                <div key={index} className="flex gap-4 text-checkmeeting-main">
+                  <Icon />
+                  <span className="">{name}</span>
+                </div>
               ))}
+            </div>
+            <div className="text-checkmeeting-main italic mb-6">
+              *Estos ingredientes pueden no estar presentes en todas las salsas,
+              pero si en las pastas
             </div>
             <span className="font-bold">
               Por favor pregunta si existe alguna restriccion alimentaria.
